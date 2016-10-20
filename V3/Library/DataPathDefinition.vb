557067555fa6516377947c044286082d2be065a6
cong_thanh_nguyen <makotovnjp@gmail.com>
2016-10-20 02:57:58 +0100

    #1 complete DataPathDefinition Class

--- V3/Library/DataPathDefinition.vb
+++ V3/Library/DataPathDefinition.vb
@@ -6,8 +6,14 @@
     Private Const C_TRADE_FOLDER_NAME As String = "取引情報"
     Private Const C_TEMPLATE_FOLDER_NAME As String = "Template情報"
 
+    'Template情報 Folder内のFile
+    Private Const C_TEMPLATE_ZAIKO_FILE_NAME As String = "Template_在庫.xlsx"
+    Private Const C_TEMPLATE_SYUKA_FILE_NAME As String = "Template_出荷.xlsx"
+    Private Const C_TEMPLATE_NYUKA_FILE_NAME As String = "Template_入荷.xlsx"
+
     '取引情報 以下のFolder
     Private Const C_SHIIRE_FOLDER_NAME As String = "仕入れ情報"
+    Private Const C_CUSTOMER_FOLDER_NAME As String = "お客様"
 
 
     Public Shared ReadOnly Property GetRootDataPath() As String
@@ -49,12 +55,60 @@
 
 #End Region
 
+#Region "Template情報Folder内のFile"
+    ''' <summary>
+    ''' Template在庫情報
+    ''' </summary>
+    ''' <returns></returns>
+    Public Shared ReadOnly Property GetTemplateZaikoPath() As String
+        Get
+            Return GetTemplateDataPath() + "\" + C_TEMPLATE_ZAIKO_FILE_NAME
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Template出荷情報
+    ''' </summary>
+    ''' <returns></returns>
+    Public Shared ReadOnly Property GetTemplateSyukaPath() As String
+        Get
+            Return GetTemplateDataPath() + "\" + C_TEMPLATE_SYUKA_FILE_NAME
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Template入荷情報
+    ''' </summary>
+    ''' <returns></returns>
+    Public Shared ReadOnly Property GetTemplateNyukaPath() As String
+        Get
+            Return GetTemplateDataPath() + "\" + C_TEMPLATE_NYUKA_FILE_NAME
+        End Get
+    End Property
+
+#End Region
+
 #Region "取引情報以下のFolder"
+    ''' <summary>
+    ''' 仕入れ情報のFolderパス
+    ''' </summary>
+    ''' <returns></returns>
     Public Shared ReadOnly Property GetShiireDataPath() As String
         Get
             Return GetTradeDataPath() + "\" + C_SHIIRE_FOLDER_NAME
         End Get
     End Property
+
+    ''' <summary>
+    ''' お客様情報のFolder
+    ''' </summary>
+    ''' <returns></returns>
+    Public Shared ReadOnly Property GetCustomerDataPath() As String
+        Get
+            Return GetTradeDataPath() + "\" + C_CUSTOMER_FOLDER_NAME
+        End Get
+    End Property
+
 #End Region
 
 End Class
