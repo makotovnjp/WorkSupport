fba7fcd4bb9579993dbb06a8d73643cccd66b904
cong_thanh_nguyen <makotovnjp@gmail.com>
2016-10-27 06:46:12 +0100

    #8 Add FileOpener Class

--- V3/Applications/InputGoodsArrivalSchedule.vb
+++ V3/Applications/InputGoodsArrivalSchedule.vb
@@ -236,7 +236,8 @@ Public Class InputGoodsArrivalSchedule
     ''' <returns></returns>
     ''' <remarks></remarks>
     Public Function ReadFile() As Integer
-        Dim OpenFileDialog As New OpenFileDialog()
+        Dim fileOpenner As New FileOpener
+        Dim openFileName As String = ""
 
         'Data格納変数
         Dim product_name As String = ""     '品名
@@ -262,58 +263,20 @@ Public Class InputGoodsArrivalSchedule
         today = DateTime.Today
         display_today = today.Year.ToString + "/" + Microsoft.VisualBasic.Right("0" & today.Month.ToString, 2) + "/" + today.Day.ToString
 
-        OpenFileDialog.Title = "ファイルを選択してください。"
+        openFileName = fileOpenner.GetFileFromDialog()
 
-        ' 初期表示するディレクトリを設定する
-        OpenFileDialog.InitialDirectory = "C:\"
-
-        ' ファイルのフィルタを設定する
-        OpenFileDialog.Filter = "Excel ファイル|*.xlsx;*.xls|すべてのファイル|*.*"
-
-        ' ファイルの種類 の初期設定を 2 番目に設定する (初期値 1)
-        OpenFileDialog.FilterIndex = 1
-
-        ' ダイアログボックスを閉じる前に現在のディレクトリを復元する (初期値 False)
-        OpenFileDialog.RestoreDirectory = True
-
-        ' 複数のファイルを選択可能にする (初期値 False)
-        OpenFileDialog.Multiselect = False
-
-        ' [ヘルプ] ボタンを表示する (初期値 False)
-        OpenFileDialog.ShowHelp = True
-
-        ' [読み取り専用] チェックボックスを表示する (初期値 False)
-        OpenFileDialog.ShowReadOnly = True
-
-        ' [読み取り専用] チェックボックスをオンにする (初期値 False)
-        OpenFileDialog.ReadOnlyChecked = True
-
-        ' 存在しないファイルを指定した場合は警告を表示する (初期値 True)
-        OpenFileDialog.CheckFileExists = True
-
-        ' 存在しないパスを指定した場合は警告を表示する (初期値 True)
-        OpenFileDialog.CheckPathExists = True
-
-        ' 拡張子を指定しない場合は自動的に拡張子を付加する (初期値 True)
-        OpenFileDialog.AddExtension = True
-
-        ' 有効な Win32 ファイル名だけを受け入れるようにする (初期値 True)
-        OpenFileDialog.ValidateNames = True
-
-        If OpenFileDialog.ShowDialog() = DialogResult.OK Then
-
-
+        If openFileName <> "" Then
             '入力ファイルの妥当性をチェックする
-            If CheckFormatInputFile(OpenFileDialog.FileName) = ARRSCHD_OK Then
+            If CheckFormatInputFile(openFileName) = ARRSCHD_OK Then
                 app = CreateObject("Excel.Application")
                 app.Visible = False
                 app.DisplayAlerts = False
 
                 'File Open
-                If IO.File.Exists(OpenFileDialog.FileName) Then 'Fileが存在する
-                    book = app.Workbooks.Open(OpenFileDialog.FileName)
+                If IO.File.Exists(openFileName) Then 'Fileが存在する
+                    book = app.Workbooks.Open(openFileName)
                 Else
-                    MsgBox("File:" + OpenFileDialog.FileName + "が存在しない")
+                    MsgBox("File:" + openFileName + "が存在しない")
                     Return ARRSCHD_ERROR
                 End If
 
---
+++ V3/Library/FileOpener.vb
@@ -0,0 +1,59 @@
+﻿'File Openするクラス
+'まずはOpenFileDialogを操作するクラスとして使う
+
+Public Class FileOpener
+    ''' <summary>
+    ''' FileDialogで選択したFileのFile名
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function GetFileFromDialog() As String
+        Dim filename As String = ""
+
+        Dim OpenFileDialog As New OpenFileDialog()
+
+        OpenFileDialog.Title = "ファイルを選択してください。"
+
+        ' 初期表示するディレクトリを設定する
+        OpenFileDialog.InitialDirectory = "C:\"
+
+        ' ファイルのフィルタを設定する
+        OpenFileDialog.Filter = "Excel ファイル|*.xlsx;*.xls|すべてのファイル|*.*"
+
+        ' ファイルの種類 の初期設定を 2 番目に設定する (初期値 1)
+        OpenFileDialog.FilterIndex = 1
+
+        ' ダイアログボックスを閉じる前に現在のディレクトリを復元する (初期値 False)
+        OpenFileDialog.RestoreDirectory = True
+
+        ' 複数のファイルを選択可能にする (初期値 False)
+        OpenFileDialog.Multiselect = False
+
+        ' [ヘルプ] ボタンを表示する (初期値 False)
+        OpenFileDialog.ShowHelp = True
+
+        ' [読み取り専用] チェックボックスを表示する (初期値 False)
+        OpenFileDialog.ShowReadOnly = True
+
+        ' [読み取り専用] チェックボックスをオンにする (初期値 False)
+        OpenFileDialog.ReadOnlyChecked = True
+
+        ' 存在しないファイルを指定した場合は警告を表示する (初期値 True)
+        OpenFileDialog.CheckFileExists = True
+
+        ' 存在しないパスを指定した場合は警告を表示する (初期値 True)
+        OpenFileDialog.CheckPathExists = True
+
+        ' 拡張子を指定しない場合は自動的に拡張子を付加する (初期値 True)
+        OpenFileDialog.AddExtension = True
+
+        ' 有効な Win32 ファイル名だけを受け入れるようにする (初期値 True)
+        OpenFileDialog.ValidateNames = True
+
+        If OpenFileDialog.ShowDialog() = DialogResult.OK Then
+            filename = OpenFileDialog.FileName
+        End If
+
+        Return filename
+    End Function
+
+End Class
